b4e1cbd8244fb8723b965bded2c714143c368a6f
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2021-08-11 21:27:14 +0700

    [fix] update for blog view

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -147,70 +147,134 @@
             <div class="member" data-aos="fade-up" data-aos-delay="100">
               <div class="member-img">
                 <img src="{{ asset('img/team/team-1.jpg') }}" class="img-fluid" alt="">
-                <div class="social">
+                <!-- <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
                   <a href=""><i class="bi bi-instagram"></i></a>
                   <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
+                </div> -->
               </div>
               <div class="member-info">
-                <h4>Walter White</h4>
-                <span>Chief Executive Officer</span>
+                <h4>The standard Lorem Ipsum passage, used since the 1500s</h4>
+                <div class="d-flex align-items-center justify-content-between my-3">
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-user"></i>
+                    <span class="ml-1">Admin</span>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-time"></i>
+                    <span class="ml-1">25 Agustus 2021</span>
+                  </div>
+                </div>
+                <hr>
+                <p class="font-normal">
+                  {{ \Illuminate\Support\Str::limit('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 225, '...') }}
+                </p>
+                <div>
+                  <button class="d-block ml-auto btn btn-brand float-right">Read More</button>
+                </div>
               </div>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="200">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
               <div class="member-img">
-                <img src="{{ asset('img/team/team-2.jpg') }}" class="img-fluid" alt="">
-                <div class="social">
+                <img src="{{ asset('img/team/team-1.jpg') }}" class="img-fluid" alt="">
+                <!-- <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
                   <a href=""><i class="bi bi-instagram"></i></a>
                   <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
+                </div> -->
               </div>
               <div class="member-info">
-                <h4>Sarah Jhonson</h4>
-                <span>Product Manager</span>
+                <h4>The standard Lorem Ipsum passage, used since the 1500s</h4>
+                <div class="d-flex align-items-center justify-content-between my-3">
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-user"></i>
+                    <span class="ml-1">Admin</span>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-time"></i>
+                    <span class="ml-1">25 Agustus 2021</span>
+                  </div>
+                </div>
+                <hr>
+                <p class="font-normal">
+                  {{ \Illuminate\Support\Str::limit('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 225, '...') }}
+                </p>
+                <div>
+                  <button class="d-block ml-auto btn btn-brand float-right">Read More</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div class="member-img">
+                <img src="{{ asset('img/team/team-1.jpg') }}" class="img-fluid" alt="">
+                <!-- <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+              <div class="member-info">
+                <h4>The standard Lorem Ipsum passage, used since the 1500s</h4>
+                <div class="d-flex align-items-center justify-content-between my-3">
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-user"></i>
+                    <span class="ml-1">Admin</span>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-time"></i>
+                    <span class="ml-1">25 Agustus 2021</span>
+                  </div>
+                </div>
+                <hr>
+                <p class="font-normal">
+                  {{ \Illuminate\Support\Str::limit('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 225, '...') }}
+                </p>
+                <div>
+                  <button class="d-block ml-auto btn btn-brand float-right">Read More</button>
+                </div>
               </div>
             </div>
           </div>
 
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="300">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
               <div class="member-img">
-                <img src="{{ asset('img/team/team-3.jpg') }}" class="img-fluid" alt="">
-                <div class="social">
+                <img src="{{ asset('img/team/team-1.jpg') }}" class="img-fluid" alt="">
+                <!-- <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
                   <a href=""><i class="bi bi-instagram"></i></a>
                   <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
+                </div> -->
               </div>
               <div class="member-info">
-                <h4>William Anderson</h4>
-                <span>CTO</span>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-            <div class="member" data-aos="fade-up" data-aos-delay="400">
-              <div class="member-img">
-                <img src="{{ asset('img/team/team-4.jpg') }}" class="img-fluid" alt="">
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
+                <h4>The standard Lorem Ipsum passage, used since the 1500s</h4>
+                <div class="d-flex align-items-center justify-content-between my-3">
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-user"></i>
+                    <span class="ml-1">Admin</span>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <i class="bx bx-time"></i>
+                    <span class="ml-1">25 Agustus 2021</span>
+                  </div>
                 </div>
-              </div>
-              <div class="member-info">
-                <h4>Amanda Jepson</h4>
-                <span>Accountant</span>
+                <hr>
+                <p class="font-normal">
+                  {{ \Illuminate\Support\Str::limit('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 225, '...') }}
+                </p>
+                <div>
+                  <button class="d-block ml-auto btn btn-brand float-right">Read More</button>
+                </div>
               </div>
             </div>
           </div>
