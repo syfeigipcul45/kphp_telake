e932678c9a45a82ea88c76e27282155ed332ebe0
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2021-10-03 00:01:22 +0700

    [fix] update changa dummy data

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -138,48 +138,48 @@
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
           <div class="icon-box">
             <div class="icon"><i class="bx bxl-dribbble"></i></div>
-            <h4><a href="">Lorem Ipsum</a></h4>
-            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            <h4><a href="">Pemerintah Prov. Kalimantan Timur</a></h4>
+            <p>Website resmi Pemerintah Provinsi Kalimantan Timur "Berani untuk Kaltim Berdaulat"</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-file"></i></div>
-            <h4><a href="">Sed ut perspiciatis</a></h4>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            <div class="icon"><i class="bx bx-tree"></i></div>
+            <h4><a href="">Kementerian Lingkungan Hidup dan Kehutanan</a></h4>
+            <p>Website resmi Kementerian Lingkungan Hidup dan Kehutanan Republik Indonesia</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-tachometer"></i></div>
-            <h4><a href="">Magni Dolores</a></h4>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            <h4><a href="">KPH Indonesia</a></h4>
+            <p>Sinpasdok KPH+ Tersebar Di Tingkat Tapak Terkendali di Pusat</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-world"></i></div>
-            <h4><a href="">Nemo Enim</a></h4>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            <h4><a href="">Form Permohonan Data</a></h4>
+            <p>Formulir Permohonan Data dan Informasi Dinas Kehutanan Provinsi Kalimantan Timur</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="200">
           <div class="icon-box">
             <div class="icon"><i class="bx bx-slideshow"></i></div>
-            <h4><a href="">Dele cardo</a></h4>
-            <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+            <h4><a href="">SI-PESAN ANTAR</a></h4>
+            <p>Sistem Informasi, Pelaporan, dan Surat Keputusan Pengada dan atau Pengedar Benih dan atau Bibit Terdaftar Online</p>
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="300">
           <div class="icon-box">
-            <div class="icon"><i class="bx bx-arch"></i></div>
-            <h4><a href="">Divera don</a></h4>
-            <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+            <div class="icon"><i class="bx bx-tree"></i></div>
+            <h4><a href="">Form Pengaduan Masyarakat</a></h4>
+            <p>Formulir Pengaduan Masyarakat Dinas Kehutanan Provinsi Kalimantan Timur</p>
           </div>
         </div>
 
