41d72edf3ef0b5015a29572285ca81b5ae973a23
syfeigipcul45 <63193446+syfeigipcul45@users.noreply.github.com>
2021-11-04 06:29:59 +0800

    dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Document;
+use App\Models\Post;
+use App\Models\Seed;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard.dashboard.index');
+        $data['news_total'] = Post::count();
+        $data['product_total'] = Seed::count();
+        $data['doc_total'] = Document::count();
+        return view('dashboard.dashboard.index', $data);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>PHP Kendilo - Login</title>
+    <title>KPHP Kendilo - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('_dashboard/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
